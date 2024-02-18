009ea5c58f4c7290e118b23ca12dd9cdaea61572
realbabish <110745063+realbabish@users.noreply.github.com>
2024-02-18 14:48:10 -0500

    Stack Data Structure in Python
    This module of the Python file includes the low-level implementation of Stack Data Structure in Python. From Pushing a value into the stack by appending a new array Object and popping the top item to viewing the top and bottom elements of the stack.

---
+++ Stack.py
@@ -0,0 +1,52 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        print(len(self.items) == 0)
+        return len(self.items) == 0
+
+    def push(self, value):
+        self.items.append(value)
+
+    def pop(self):
+        index = len(self.items) - 1
+        self.items.pop()
+
+    def top_element(self):
+        index = len(self.items) - 1
+        return print("The top element of the stack is: ", self.items[index])
+    def peek(self):
+        return print("The bottom of the stack ; First Element is: ", self.items[0])
+
+    def length(self):
+        index = len(self.items)
+        return print("The number of units in the stack is: ", index)
+
+
+Stack = Stack()
+Stack.push(1)
+Stack.push(2)
+Stack.push(3)
+Stack.push(4)
+Stack.push(5)
+Stack.push(6)
+
+print("Stack after 6 pushes")
+Stack.peek()
+Stack.top_element()
+Stack.length()
+
+print("Top of the stack after a pop")
+Stack.pop()
+Stack.peek()
+Stack.top_element()
+Stack.length()
+
+print("Top of the stack after the 2nd pop")
+Stack.pop()
+Stack.peek()
+Stack.top_element()
+Stack.length()
+
+
